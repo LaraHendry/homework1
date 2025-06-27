edcd52f1fa2cca01ab953bd097ff24c4b95ffb32
LaraHendry <lara.hendry@xdesign.com>
2025-06-27 12:06:05 +0100

    reducing page loads

--- tests/cf-website/ui.homepage.spec.ts
+++ tests/cf-website/ui.homepage.spec.ts
@@ -2,30 +2,26 @@ import { test, expect } from "../base";
 
 test.describe("Smoke check the CreateFuture website UI", () => {
 
-    test.beforeEach(async ({ homepage }) => {
-    const page = homepage.page;
+
+  
+
+  test("Open homepage and check logo", async ({ homepage, commonFunctions }) => {
+    //const page = homepage.page;
+      const page = homepage.page;
     await homepage.goToHomePage();
     await homepage.viewCookieConsentMessage();
     await expect(homepage.getCookieConsentMessage()).toBeVisible();
     await page.getByRole("button", { name: "Decline" }).click();
-  });
-
-  test("Open homepage and check logo", async ({ homepage }) => {
-    const page = homepage.page;
-    
+  
     await test.step("Check CreateFuture logo", async () => {
       await expect(
         page.getByRole("link", { name: "CreateFuture Logo black text" }),
       ).toBeVisible();
       await expect(homepage.aboutNav).toBeVisible();
     });
-  });
-
-  test("Open homepage and check upper UI", async ({ homepage, commonFunctions }) => {
-    const page = homepage.page;
 
     await test.step("Check 'About' tab", async () => {
-     await expect(homepage.aboutNav).toBeVisible();
+      await expect(homepage.aboutNav).toBeVisible();
       await homepage.expandTab(homepage.aboutNav, homepage.whoWeAreNav);
       await expect(homepage.whoWeAreNav).toBeVisible();
       await homepage.expandTab(homepage.aboutNav, homepage.missionsAndValuesNav);
@@ -34,15 +30,10 @@ test.describe("Smoke check the CreateFuture website UI", () => {
       await expect(homepage.aiEnablementNav).toBeVisible();
       
   });
-  });
-
-  test("Open homepage and check horizontal scroll banner", async ({
-    homepage,
-  }) => {
-    const page = homepage.page;
-    const bannerTimerMs = 1000;
 
     await test.step("Homepage displays first banner", async () => {
+        const page = homepage.page;
+    const bannerTimerMs = 1000;
       await expect(homepage.firstBanner).toBeVisible();
       await page.waitForTimeout(bannerTimerMs);
       await expect(homepage.secondBanner).toBeVisible();
